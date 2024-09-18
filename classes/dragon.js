cc71b29717e8029dd7c3fd625083668bd6e9416d
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-09-18 15:27:52 -0700

    finished

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,29 @@
-// Your code here 
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    let listOfDragons = [];
+    for (let dragon of dragons) {
+      listOfDragons.push(dragon.name);
+    }
+    return listOfDragons;
+  }
+ }
+
+//const puff = new Dragon("Puff", "green");
+//console.log(puff);
+//console.log(puff.breathesFire());
+
+//const puff = new Dragon("Puff", "green");
+//const toothless = new Dragon("Toothless", "black");
+//console.log(Dragon.getDragons(puff, toothless));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
